9b122e6b411643bad74841493cd0a8994f406f62
Chhavi Mittal <chhavi.mitmpl2025@learner.manipal.edu>
2026-07-08 00:20:39 +0530

    solved climbing stairs through recursion

---
+++ climbing_stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+unordered_map<int,int> memo;
+    int climbStairs(int n) {
+        
+        if(n==0||n==1){
+            return 1;
+        }
+        if(memo.count(n)){
+            return memo[n];
+        }
+        memo[n]=climbStairs(n-1)+climbStairs(n-2);
+        
+        return memo[n];
+        
+    }
+};
